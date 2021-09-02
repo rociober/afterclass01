5f021e6540c04afc874e934c818c981bcc55f45d
rociober <rocioberardi25@gmail.com>
2021-09-01 21:57:26 -0300

    NavBar

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import './App.css';
-
+import NavBar from './component/NavBar.js';
+ 
 function App() {
-  return (
+  return (  
     <div className="App">
       <header className="App-header">
       
       </header>
+      return( <NavBar /> )
     </div>
   );
 }
+
+
 
 export default App;
---
+++ src/component/NavBar.js
@@ -0,0 +1,14 @@
+import './NavBar.css'
+
+const NavBar=() =>
+{
+    return (
+        <div className="body" id="menu">
+            <button>Nosotros</button>
+            <button>tratamientos</button>
+            <button>¿Como llegar?</button>
+            <button>Contacto</button>
+        </div>
+    )}
+
+export default NavBar;
